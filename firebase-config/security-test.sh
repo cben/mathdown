4ff7214c0973b842c5606f486ebbb51ac23354b7
Beni Cherniavsky-Paskin <cben@users.sf.net>
2013-09-10 17:57:55 -0700

    I've set Firebase's security to make firepads IDs secret.
    [This commit doesn't do anything by itself, just documents the config change]

---
+++ firebase-config/security-test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# TODO: learn to write this kind of theng in Node.js.
+
+fails=0
+
+firebase=mathdown-alpha.firebaseio.com
+# Can't read list of pads
+curl --silent --head -X GET https://$firebase/firepads.json | grep 403 || let fails++
+# Can read pad with known name
+curl --silent --head -X GET https://$firebase/firepads/help/history.json | grep 200 || let fails++
+curl --silent --head -X GET https://$firebase/firepads/help/checkpoint.json | grep 200 || let fails++
+curl --silent --head -X GET https://$firebase/firepads/help/users.json | grep 200 || let fails++
+
+echo
+if [ $fails != 0 ]; then
+  echo "$fails FAILURES"
+else
+  echo "PASS"
+fi
+exit $fails
