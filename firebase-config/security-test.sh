26d6ca90c0842e27e74d3bd986b847a801ce8ec3
Beni Cherniavsky-Paskin <cben@users.sf.net>
2014-04-01 16:34:32 -0700

    Fix security test to allow 40X and not only 403. Currently firebase returns 401 Unauthorized.

--- firebase-config/security-test.sh
+++ firebase-config/security-test.sh
@@ -5,7 +5,7 @@ firebase=mathdown.firebaseio.com
 
 fails=0
 # Can't read list of pads
-curl --silent --head -X GET https://$firebase/firepads.json | grep 403 || let fails++
+curl --silent --head -X GET https://$firebase/firepads.json | grep '40[0-9]' || let fails++
 # Can read pad with known name
 curl --silent --head -X GET https://$firebase/firepads/help/history.json | grep 200 || let fails++
 curl --silent --head -X GET https://$firebase/firepads/help/checkpoint.json | grep 200 || let fails++
